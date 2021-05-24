3bfb5f14e2456285cf9313c7c2c86321fb93c497
IgorFNegrizoli <igor.negrizoli@gmail.com>
2021-05-24 20:34:22 -0300

    Added initial screen

---
+++ app/src/main/java/com/example/oscartracker/dashboard.java
@@ -0,0 +1,18 @@
+package com.example.oscartracker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        //Typeface type = Typeface.createFromAsset(getAssets(),"fonts/arial.ttf");
+        //TextView.setTypeface(type);
+
+    }
+}
