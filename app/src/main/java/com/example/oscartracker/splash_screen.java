955eb5ce7f344fed47640292d628d2005cc60077
IgorFNegrizoli <igor.negrizoli@gmail.com>
2021-07-22 10:31:14 -0300

    Splash screen is now functional

--- app/src/main/java/com/example/oscartracker/splash_screen.java
+++ app/src/main/java/com/example/oscartracker/splash_screen.java
@@ -2,7 +2,9 @@ package com.example.oscartracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class splash_screen extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class splash_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        //Splash Screen duration
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(splash_screen.this, dashboard.class));
+                finish();
+            }
+        }, secondsDelayed * 3000);
     }
 }
