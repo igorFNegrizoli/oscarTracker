1b6cfe8c7fa3526e28778b9e4109aaf93c93107a
MahatKC <mkarvat@gmail.com>
2021-07-22 08:40:07 -0300

    Finalização do arquivo dbHelper

--- app/src/main/java/database/databaseHelper.java
+++ app/src/main/java/database/databaseHelper.java
@@ -1,19 +1,15 @@
 package database;
-
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-
 public class databaseHelper extends SQLiteOpenHelper {
-
     public static int VERSION = 1;
     public static String NOME_DB = "OscarTrackerDB";
     public static String TABELA_FILME = "filme";
     public static String TABELA_CATEGORIA_FILME = "categoriaFilme";
     public static String TABELA_CATEGORIA = "categoria";
-
 
     public databaseHelper(Context context) {
         super(context, NOME_DB, null, VERSION);
@@ -34,24 +30,26 @@ public class databaseHelper extends SQLiteOpenHelper {
         String sql_create_categoria_filme = "CREATE TABLE IF NOT EXISTS " + TABELA_CATEGORIA_FILME +"("+
                 " id_categoria INTEGER REFERENCES categoria(id),"+
                 " id_filme INTEGER REFERENCES filme(id),"+
-                " PRIMARY KEY(id_categoria, id_filme),"+
                 " rating INTEGER DEFAULT 0," +
-                " indicado TEXT"+
-                " caminhoImagemIndicado TEXT"+
+                " indicado TEXT,"+
+                " caminhoImagemIndicado TEXT," +
+                " CONSTRAINT pk_cat_film PRIMARY KEY (id_categoria, id_filme)"+
                 ");";
 
         String sql_create_categoria = "CREATE TABLE IF NOT EXISTS " + TABELA_CATEGORIA +"("+
                 " id INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 " nome TEXT NOT NULL, " +
-                " selecionada INTEGER DEFAULT 1"+
+                " selecionada INTEGER DEFAULT 1,"+
                 " winner INTEGER REFERENCES filme(id),"+
                 " runnerUp INTEGER REFERENCES filme(id),"+
                 " wantsToWin INTEGER REFERENCES filme(id),"+
-                " actualWinner INTEGER REFERENCES filme(id),"+
+                " actualWinner INTEGER REFERENCES filme(id)"+
                 ");";
 
         try {
-            //db.execSQL( sql );
+            db.execSQL(sql_create_filme);
+            db.execSQL(sql_create_categoria_filme);
+            db.execSQL(sql_create_categoria);
             Log.i("INFO DB", "Sucesso ao criar a tabela" );
         }catch (Exception e){
             Log.i("INFO DB", "Erro ao criar a tabela" + e.getMessage() );
@@ -62,10 +60,14 @@ public class databaseHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
-        //String sql = "DROP TABLE IF EXISTS " + TABELA_PESSOA + " ;" ;
+        String sql_create_filme = "DROP TABLE IF EXISTS " + TABELA_FILME + " ;";
+        String sql_create_categoria_filme = "DROP TABLE IF EXISTS " + TABELA_CATEGORIA_FILME + " ;";
+        String sql_create_categoria = "DROP TABLE IF EXISTS " + TABELA_CATEGORIA + " ;";
 
         try {
-            db.execSQL( sql );
+            db.execSQL(sql_create_filme);
+            db.execSQL(sql_create_categoria_filme);
+            db.execSQL(sql_create_categoria);
             onCreate(db);
             Log.i("INFO DB", "Sucesso ao atualizar App" );
         }catch (Exception e){
