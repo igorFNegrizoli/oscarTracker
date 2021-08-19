026bcdb7ad9dc54c469c82ac80484ffd18e27d52
IgorFNegrizoli <igor.negrizoli@gmail.com>
2021-08-19 12:20:55 -0300

    Fixed recycler view cropping on the movies_list activity, made changes on style and changed the status bar style. Should have done multiple commits

--- app/src/main/java/com/example/oscartracker/movies_list/ActivityMoviesList.java
+++ app/src/main/java/com/example/oscartracker/movies_list/ActivityMoviesList.java
@@ -1,5 +1,6 @@
 package com.example.oscartracker.movies_list;
 
+import android.app.ActionBar;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,11 +27,22 @@ public class ActivityMoviesList extends AppCompatActivity {
     private final consultas consulta = new consultas();
     private List<ModelMoviesList> listMovies = new ArrayList<>();
     private RecyclerView recyclerView;
+    //View decorView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies_list);
+
+        /*decorView = getWindow().getDecorView();
+        decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener(){
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+                if(visibility == 0) decorView.setSystemUiVisibility(hideSystemBars());
+            }
+        });
+        */
+
         recyclerView = findViewById(R.id.recyclerView);
         this.populateMovies();
         AdapterMoviesList adapter = new AdapterMoviesList(listMovies);
@@ -63,7 +75,24 @@ public class ActivityMoviesList extends AppCompatActivity {
                 )
         );
     }
+/*
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if(hasFocus){
+            decorView.setSystemUiVisibility(hideSystemBars());
+        }
+    }
 
+    private int hideSystemBars(){
+        return View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+    }
+*/
     public void populateMovies(){
         databaseHelper db = new databaseHelper(this);
         le = db.getReadableDatabase();
