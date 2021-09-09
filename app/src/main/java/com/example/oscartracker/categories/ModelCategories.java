0188f7f0f996cf635f0818730c826db17fb6f80d
MahatKC <mkarvat@gmail.com>
2021-09-09 10:48:16 -0300

    model categories

--- app/src/main/java/com/example/oscartracker/categories/ModelCategories.java
+++ app/src/main/java/com/example/oscartracker/categories/ModelCategories.java
@@ -1,4 +1,46 @@
 package com.example.oscartracker.categories;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.List;
+
+import database.consultas;
+import database.databaseHelper;
+
 public class ModelCategories {
+    List<String> categories;
+    List<ModelCategoryMovies> categoryMovies;
+
+    public ModelCategories(Context context) {
+        SQLiteDatabase le;
+        final consultas consulta = new consultas();
+        databaseHelper db = new databaseHelper(context);
+        le = db.getReadableDatabase();
+
+        this.categories = consulta.readSelectedCategories(le);
+
+        for(String categoria: this.categories){
+            ModelCategoryMovies categoryMovies = new ModelCategoryMovies(le, categoria);
+            this.categoryMovies.add(categoryMovies);
+        }
+
+        le.close();
+    }
+
+    public List<String> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<String> categories) {
+        this.categories = categories;
+    }
+
+    public List<ModelCategoryMovies> getCategoryMovies() {
+        return categoryMovies;
+    }
+
+    public void setCategoryMovies(List<ModelCategoryMovies> categoryMovies) {
+        this.categoryMovies = categoryMovies;
+    }
 }
---
+++ app/src/main/java/com/example/oscartracker/categories/ModelCategoryMovies.java
@@ -0,0 +1,29 @@
+package com.example.oscartracker.categories;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Pair;
+
+import java.util.List;
+
+import database.consultas;
+import database.databaseHelper;
+import utils.Utils;
+
+public class ModelCategoryMovies {
+    List<Pair<String,Integer>> filmesNotas;
+
+    public List<Pair<String, Integer>> getFilmesNotas() {
+        return filmesNotas;
+    }
+
+    public void setFilmesNotas(List<Pair<String, Integer>> filmesNotas) {
+        this.filmesNotas = filmesNotas;
+    }
+
+    public ModelCategoryMovies(SQLiteDatabase le, String category) {
+        final consultas consulta = new consultas();
+
+        this.filmesNotas =  consulta.readCategoriaFilmesNotas(le, category);
+    }
+}
